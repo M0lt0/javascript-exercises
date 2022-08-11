7afe3e4e408eb4caf57d85c964996299effd23c5
molto <bodymgahed@yahoo.com>
2022-08-11 06:03:26 +0200

    all done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function (arr, ...items) {
-  arr = arr.filter((item) => {
-    item.indexOf(items) === -1;
+const removeFromArray = function (...arg) {
+  const array = arg[0];
+  const newArr = [];
+  array.forEach((elm) => {
+    if (!arg.includes(elm)) {
+      newArr.push(elm);
+    }
   });
+  return newArr;
 };
 
 // Do not edit below this line
